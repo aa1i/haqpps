5160fa00d56f5d2111a75511676a3fe1a6a60faa
John D. Isham <isham.john@gmail.com>
2017-03-17 14:23:39 -0400

    updated pyaudio_jdi for initial release; updated readme

--- pyaudio_jdi.py
+++ pyaudio_jdi.py
@@ -1,12 +1,36 @@
+
+# some help from
+#https://gist.githubusercontent.com/Jach/6361147/raw/c91a311f9d3e83bb11d2d3f65457650e9b624965/audio_trigger.py
+'''
+Requires PyAudio and Numpy.
+
+Windows users:
+win32 Python 2.7: http://www.python.org/ftp/python/2.7.3/python-2.7.3.msi
+Numpy: http://sourceforge.net/projects/numpy/files/NumPy/1.7.1/numpy-1.7.1-win32-superpack-python2.7.exe/download
+PyAudio: http://people.csail.mit.edu/hubert/pyaudio/packages/pyaudio-0.2.7.py27.exe
+'''
 import pyaudio
 import wave
+import numpy
+import math
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+RATE = 192000
+RECORD_SECONDS = 15
+#WAVE_OUTPUT_FILENAME = "output.wav"
+INHIBITION=10
+#INHIBITION=60 
+#INHIBITION=480
+
+VERBOSE=2
+
+left_channel = 0
+right_channel = 1
+
+# window to flag a missed pulse - 1.05 seconds, in integer samples
+MISSED_PULSE = 105 * RATE / 100
 
 p = pyaudio.PyAudio()
 
@@ -18,21 +42,159 @@ stream = p.open(format=FORMAT,
 
 print("* recording")
 
-frames = []
+#frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+left_max=0.0  # Watch Under Test
+right_max=0.0 # PPS
+
+sample_count = 0
+
+first_right=0
+first_left=0
+
+right_count=0
+left_count=0
+
+last_right=0
+last_left=0
+TRIGGER_WIDTH=RATE/4
+
+left_thresh=0.5
+right_thresh=0.5
+
+#avg_rate=float(RATE)
+avg_rate=192004.388773 # from a previous run
+
+tickfile=open('ticks.txt','w')
+offsfile=open('offset.txt','w',1) # line buffered
+
+sw_avg=[]
+last_offset=0.0
+last_offset_sample=0
+  
+#for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+while True:
+  try:
     data = stream.read(CHUNK)
-    frames.append(data)
 
-print("* done recording")
+    # append to output file
+    #frames.append(data)
+
+    # decode/deinterleave soundard samples
+    decoded = numpy.fromstring(data,"Int16")
+    # Normalize by int16 max (32767) for convenience, also converts everything to floats
+    normed_samples = decoded / float(numpy.iinfo(numpy.int16).max)
+
+    left_samples = normed_samples[left_channel::2]
+    right_samples = normed_samples[right_channel::2]
+
+    # track maximum amplitude per channel
+    tmp=max(abs(j) for j in left_samples)
+    left_max = max(left_max, tmp)
+
+    tmp=max(abs(j) for j in right_samples)
+    right_max=max(right_max ,tmp)
+
+    # detect pulses
+    # we can probable replace sample_num with right below,
+    # but since we use the same index for the left/tic data,
+    # sample_num is more straightforward
+    sample_num = 0
+    for right  in  right_samples:
+        # look for PPS reference pulses on right channel
+        if (abs(right_samples[sample_num]) > right_thresh):
+          if ((0 == last_right) or (sample_count -last_right > TRIGGER_WIDTH)):
+              if 0 == first_right:
+                  first_right=sample_count
+              if VERBOSE >= 2:
+                  print '{0:09d} {1:f} PPS {2:7d} {3:f}'.format(
+                      sample_count,float(sample_count)/avg_rate,
+                      sample_count-last_right,float(sample_count-last_right)/avg_rate)
+              last_right= sample_count               
+              right_count+=1
+              # update sample rate referenced to PPS pulses
+              if right_count >= 10:
+                 avg_rate=float(last_right-first_right)/float(right_count-1)
+        # look for watch 'tics' on left channel
+        if (abs(left_samples[sample_num]) > left_thresh):
+          if ((0 == last_left) or (sample_count -last_left > TRIGGER_WIDTH)):
+              if 0 == first_left:
+                  first_left=sample_count
+              cur_clock=float(sample_count)/avg_rate
+
+              # check for missing PPS pulse
+              if sample_count - last_right > MISSED_PULSE:
+                  print '{0:09d} {1:f} PPS REFERENCE UNLOCK'.format(
+                      sample_count,cur_clock)
+                  # TODO - reset counters/stats!!
+                  sw_avg=[]
+                  last_right=0
+                  first_right=0
+                  right_count=0
+              else:
+                 offset=float(sample_count-last_right)/avg_rate
+                 if offset < MISSED_PULSE:
+                     if VERBOSE >=1:
+                         print '{0:09d} {1:f} tic {2:7d} {3:f}'.format(
+                             sample_count,cur_clock,
+                             sample_count-last_right,float(sample_count-last_right)/avg_rate)
+
+                     # log tick data to file
+                     tickfile.write('{0:09d} {1:f} tic {2:7d} {3:f}\n'.format(
+                         sample_count,cur_clock,
+                         sample_count-last_right,float(sample_count-last_right)/avg_rate))
+
+                     # sliding window average over inhibition period
+                     sw_avg.append(offset)
+                     if INHIBITION == len(sw_avg):
+                         avg_offset=math.fsum(sw_avg)/float(INHIBITION)
+                         print '{0:09d} {1:f} offset {2:f}'.format(
+                             sample_count,cur_clock,avg_offset)
+                         offsfile.write('{0:09d} {1:f} offset {2:f}\n'.format(
+                             sample_count,cur_clock,avg_offset))
+                         sw_avg=[]
+                         # primitive rate calc based on last two inihibition periods only
+                         # better results will be obtained from linear fit to more offset data over longer timebase
+                         if last_offset_sample > 0:
+                             rate=float(avg_offset-last_offset)*avg_rate/float(sample_count-last_offset_sample)
+                             print '{0:09d} {1:f} rate {2:e} spd {3:f} spy {4:f}'.format(
+                                 sample_count, cur_clock, rate, rate*86400.0, rate*86400.0*365.0)
+                         last_offset_sample=sample_count
+                         last_offset=avg_offset
+              last_left= sample_count               
+              left_count+=1
+        sample_num+=1
+        sample_count+=1
+
+
+  except KeyboardInterrupt: 
+      print "Caught KeyboardInterrupt" 
+      break;
+  except IOError:
+      print "Caught IOError" 
+      break;
+
+print ("* done recording")
 
 stream.stop_stream()
 stream.close()
 p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+tickfile.close()
+offsfile.close()
+
+print
+print "Avg rate:",avg_rate
+print "PPS count:",right_count
+print "tic count:",left_count
+print "max PPS ampl:",right_max
+print "max tic ampl:",left_max
+
+#wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+#wf.setnchannels(CHANNELS)
+#wf.setsampwidth(p.get_sample_size(FORMAT))
+#wf.setframerate(RATE)
+#wf.writeframes(b''.join(frames))
+#wf.close()
+
+quit()
--- readme.txt
+++ readme.txt
@@ -1,7 +1,10 @@
+background/motivation: lightning deals unleashed my inner WIS. Needed a way to measure all these watches and feed my OCD, plus use that new GPS module I had bought.
+
 goal:
- create a low cost, accurate measurement system for evaluating HAQ
+ create a low cost, accurate measurement system for evaluating HAQ watches. Improve on the precision/accuracy of video methods, and at lower cost. Make it easier than manual stopwatch. Where possible use free and/or open source tools, and Ideally something that will work on Windows and Linux. Admittedly, also a way to use the GPS module I just bought for something useful. Although it is the most expensive part of the setup (other than the watches or the computer), it is a very cost effective timebase reference.
 
 references
+  measuring haq thread
   hans moleman for the measurement technique
   igna for the affordable telephone inductive pickup
 
@@ -11,11 +14,15 @@ new
   python/pyaudio for portable solution. Linux, windows, mac?
 
 equipment
-  computer with soundcard capable of 192Khz. 16 bit fine. any gain from 24/32 bit?
+  computer with soundcard capable of 192Khz. 96/48/44.1 are probably fine too, but if 192khz available, no reason not to use it. 16 bit fine. any gain from 24/32 bit?
   inductive telephone pickup. [part number] ~$2
   stereo-to-mono splitter [not Y-cable]. [Hosa part #] ~$3-6
   Adafruit GPS [model #][plate with proto area preferred?] ~$40
+    needs power - using USB via raspberry pi. could direct wire USB or bench supply
   misc LED/resistors.
+  python program [link to source]
+
+Results
 
 problems
 
@@ -34,13 +41,17 @@ PPS pulse is AC coupled and appears as a dual sawtooth, rather than a square pul
  pickup signal is low. Boosting signal too much results in spurious samples when noise exceeds threshold. Lower signals increase noise in measurements, but this seems to have less effect on measurments.
  Hand wind pickup for better gain?
 
+  setting mics gains is essential. Would be nice to add real time plots. GnuRadio is hard to install.
+
  may need better filtering of spurious samples. time gating? effect on capture if initial sample noisy?
 
   office environment poor for GPS. PPS occasionally lost. good for testing, bad for measurement.
 
  is handling of PPS dropouts working as expected
 
- offline plots via gnuplot. Integrate in pthon?
+ offline plots via gnuplot. Integrate in python?
+
+  can't handle watches > 1 bps (yet). Precisionist 16 Hz.
 
 future
   Validate various internet time programs [watchuseek, time.is, emerald time] via direct audio.
