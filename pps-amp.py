c7d06ecf17e1c5f6dc937db5a8c712aa7173ee38
John D. Isham <isham.john@gmail.com>
2017-03-23 14:12:13 -0400

    added labels and plot close/quit

--- pps-amp.py
+++ pps-amp.py
@@ -1,21 +1,17 @@
 """
-Using Pyaudio, record sound from the audio device and plot, for 8 seconds, and display it live in a Window.
+Using Pyaudio, record sound from the audio device and display it live in a Window.
 Usage example: python pyrecplotanimation.py
 Gerald Schuller, October 2014 
 http://www.dk0tu.de/users/DL5BBN/Python_Amateur_Radio_Programs/
+
+Modified extensively 2016/Apr-May by John Isham (aa1i) <isham.john@gmail.com>
 """
 
 import pyaudio
 #import struct
-#import math
-#import array
 import numpy as np
-#import sys
-#import wave
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-#import pylab
-#import cv2
 
 CHANNELS = 2
 RATE = 192000  #Sampling Rate in Hz
@@ -26,30 +22,31 @@ WIDTH = 2      #2 bytes per sample
 
 x = np.arange(0, RATE) * 1000  / RATE      # x-array
 
+
 # http://stackoverflow.com/questions/29832055/animated-subplots-using-matplotlib
 # create a figure with two subplots
 #fig, (ax1, ax2) = plt.subplots(2,1)
 fig, (ax1, ax2) = plt.subplots(2,sharex=True)
+
+fig.canvas.set_window_title('PPS Amplitude')
+fig.suptitle('Waveform Scope')
 
 # intialize two line objects (one in each axes)
 line1, = ax1.plot([], [], lw=2)
 line2, = ax2.plot([], [], lw=2, color='r')
 line = [line1, line2]
 
-ax1.set_ylim( -1.0, 1.0)
-ax2.set_ylim( -1.0, 1.0)
-
-ax1.set_xlim(0, 1000)
-ax2.set_xlim(0, 1000)
-
-ax1.grid(True)
-ax2.grid(True)
-
-ax1.set_xlabel('Time (msec)')
-ax1.set_ylabel('Stepper')
-
-ax2.set_xlabel('Time (msec)')
+for ax in [ax1, ax2]:
+    ax.set_ylim(-1.0, 1.0)
+    ax.set_xlim(0, 1000)
+    ax.grid(True)
+    ax.set_xlabel('Time (msec)')
+    
+ax1.set_ylabel('Stepper Pulse')
 ax2.set_ylabel('PPS Reference')
+
+#ax1.set_title('axis 1 title')
+#ax2.set_title('axis 2 title')
 
 
 def animate(i):
@@ -94,17 +91,26 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
 
 print("* recording")
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
-    interval=25, blit=True)
+#try:
+
+ani = animation.FuncAnimation(fig, animate, init_func=init, interval=25, blit=True)
 plt.show()
 
-
+#except KeyboardInterrupt: 
+#    print "Caught KeyboardInterrupt" 
+#    raise
+#except IOError:
+#    print "Caught IOError" 
+#    raise
+    
 # When everything done, release the capture
 
 print("* done")
 
 #f.close()
+plt.close()
 stream.stop_stream()
 stream.close()
 p.terminate()
 
+quit()
