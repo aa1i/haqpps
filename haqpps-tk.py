047bba2f7a1cdd234e1d11efa4ddd0bbbb4b2952
John D. Isham <isham.john@gmail.com>
2017-03-23 15:34:23 -0400

    delete threads from haqpps-tk and replace with subprocs

--- haqpps-tk.py
+++ haqpps-tk.py
@@ -1,63 +1,24 @@
 #!/usr/bin/python
 
 #import Tkinter as Tk
-from Tkinter import *
-import ttk
+from tkinter import *
+from tkinter import ttk
 
 from time import sleep
 import time
 
-import threading
-
 import rate_day
+import rate_cumulative
 #import pps_amp_filt
 import subprocess
 #import popen
 
-# busy thread for testing
-class myThread (threading.Thread):
-    exitFlag=0
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-
-    def run(self):
-        print ("Starting " + self.name)
-        self.print_time(self.name, self.counter, 30)
-        #pps_amp_filt.run()
-        #subprocess.call(["python","pps_amp_filt.py"])
-        print ("Exiting " + self.name)
-
-    def print_time(self,threadName, delay, counter):
-        while counter and not myThread.exitFlag:
-            print ("%s: %s" % (threadName, time.ctime(time.time())))
-            counter -= 1
-            time.sleep(delay)
-
-class measureThread (myThread):
-    def print_time(self,threadName, delay, counter):
-        while counter and not measureThread.exitFlag:
-            print ("%s: %s" % (threadName, time.ctime(time.time())))
-            counter -= 1
-            time.sleep(delay)
-
-class scopeThread (myThread):
-    def run(self):
-        print ("Starting " + self.name)
-        #self.print_time(self.name, self.counter, 30)
-        #pps_amp_filt.run()
-        #proc = subprocess.Popen(["python","pps_amp_filt.py"])
-        #self.subprocs.append( proc )
-        print ("Exiting " + self.name)
-            
 class App:
     def __init__(self, master):
 
         # tk initialization
         self.frame = Frame(master)
-        Label(text="frame").pack()
+        Label(text="frame label").pack()
         self.frame.pack()
 
         bottom_frame = ttk.Labelframe(master, text="Actions:")
@@ -82,13 +43,26 @@ class App:
         self.test.config(state='disabled')
         self.test.pack(side=RIGHT)
 
-        Button(bottom_frame, text="Level Scope", command=self.wave_scope).pack(side=LEFT)
-        Button(bottom_frame, text="Kill Scope", command=self.wave_scope_kill).pack(side=LEFT)
-        
-        Button(bottom_frame, text="Measure", command=self.measure).pack(side=LEFT)
-        Button(bottom_frame, text="Kill Measure", command=self.measure_kill).pack(side=LEFT)
+        # watch scope buttons
+        scope_frame = ttk.Labelframe(bottom_frame, text="Scope:")
+        scope_frame.pack(side=LEFT)
+        Button(scope_frame, text="Level Scope", command=self.wave_scope).pack(side=LEFT)
+        Button(scope_frame, text="Kill Scope", command=self.wave_scope_kill).pack(side=LEFT)
 
-        Button(bottom_frame, text="Plot Rate", command=self.rate_plot).pack(side=RIGHT)
+        # measurement buttons
+        measure_frame = ttk.Labelframe(bottom_frame, text="Measure:")
+        measure_frame.pack(side=LEFT)
+        self.measure_button = ttk.Button(measure_frame, text="Measure", command=self.measure)
+        self.measure_button.config(state='enabled')
+        self.measure_button.pack(side=TOP)
+        Button(measure_frame, text="Kill Measure", command=self.measure_kill).pack(side=BOTTOM)
+
+        # plot generating buttons
+        plot_frame = ttk.Labelframe(bottom_frame, text="Plots:")
+        plot_frame.pack(side=RIGHT)
+        Button(plot_frame, text="Close Plots",     command=self.plot_kill).pack(side=RIGHT)   
+        Button(plot_frame, text="Plot Cumulative", command=self.rate_cum_plot).pack(side=RIGHT)   
+        Button(plot_frame, text="Plot Rate",       command=self.rate_plot).pack(side=RIGHT)
        
         #bottom_frame.pack(side=BOTTOM)
 
@@ -118,79 +92,77 @@ class App:
         Radiobutton(inhib_frame, text="480", variable=self.inhib_period, value=480).pack(side=LEFT)
         Radiobutton(inhib_frame, text="960", variable=self.inhib_period, value=960).pack(side=LEFT)
 
-        # initialize threads
-        self.threads=[]
-        myThread.exitFlag=0
-        measureThread.exitFlag=0
-        self.measure_threads=[]
+        # initialize sub-process lists
         self.subprocs=[]
+        self.plot_subprocs=[]
+        self.scope_subprocs=[]
         
     def say_hi(self):
-        print "hi there, everyone!"
+        print ("hi there, everyone!")
         
     def show_info(self):
-        print "watch: " + self.watch_name.get()
-        print "inhib:" , self.inhib_period.get()
+        print ("watch: " + self.watch_name.get())
+        print ("inhib:" , self.inhib_period.get())
 
     # obsolete?
     def cancel(self):
-        print "cancel STUB"
+        print ("cancel STUB")
         
     def rate_plot(self):
-        print "rate_plot START"
-        #thread1 = myThread(1, "WvScope-"+str(len(self.threads)+1), 1)
-        #thread1.start()
-        #self.threads.append(thread1)
-        rate_day.main()
-        print "rate_plot return"
+        print ("rate_plot START")
+        proc = subprocess.Popen(["python","rate_day.py"])
+        self.plot_subprocs.append( proc )
+        print ("rate_plot return")
         
+    def rate_cum_plot(self):
+        print ("cumulative_rate_plot START")
+        proc = subprocess.Popen(["python","rate_cumulative.py"])
+        self.plot_subprocs.append( proc )
+        print ("cumulative_rate_plot return")
+        
+    def plot_kill(self):
+        print ("plot_kill")
+        print ("Killing plot subprocs")
+        for p in self.plot_subprocs:
+            p.kill()
+        print ("plot subprocs done.")
+        self.plot_subprocs=[]
+
     def wave_scope(self):
-        print "wave_scope START"
-        #myThread.exitFlag=0;
-        #thread1 = myThread(1, "WvScope-"+str(len(self.threads)+1), 1)
-        #thread1 = scopeThread(1, "ScopeTh-"+str(len(self.threads)+1), 1)
-        #thread1.start()
-        #self.threads.append(thread1)
+        print ("wave_scope START")
         proc = subprocess.Popen(["python","pps_amp_filt.py"])
-        print "wave_scope return"
-        self.subprocs.append( proc )
+        print ("wave_scope return")
+        self.scope_subprocs.append( proc )
         
     def wave_scope_kill(self):
-        print "wave_scope_kill"
-        #myThread.exitFlag=1
-
-        print ("Killing subprocs")
-        for p in self.subprocs:
+        print ("wave_scope_kill")
+        print ("Killing scope subprocs")
+        for p in self.scope_subprocs:
             p.kill()
-        print ("subprocs done.")
-        self.subprocs=[]
-        #print ("Joining threads")
-        #for t in self.threads:
-        #    t.join()
-        #print ("threads done.")
-        #self.threads=[]
+        print ("scope subprocs done.")
+        self.scope_subprocs=[]
         
 
     def measure(self):
-        print "measure START"
-        measureThread.exitFlag=0;
-        thread1 = measureThread(1, "Measure-"+str(len(self.measure_threads)+1), 1)
-        thread1.start()
-        self.measure_threads.append(thread1)
+        self.measure_button.config(state='disabled')
         self.test.config(state='normal')
+        print ("measure START")
+        proc = subprocess.Popen(["python","haqpps4.py"])
+        print ("measure return")
+        self.subprocs.append( proc )
 
     def measure_kill(self):
-        print "measure_kill"
+        print ("measure_kill")
+        print ("Killing measure subprocs")
+        for p in self.subprocs:
+            p.kill()
+        print ("measure subprocs done.")
+        self.subprocs=[]
         self.test.config(state='disabled')
-        measureThread.exitFlag=1
-
-        print ("Joining threads")
-        for t in self.measure_threads:
-            t.join()
-        print ("threads done.")
-        self.measure_threads=[]
+        self.measure_button.config(state='normal')
 
     def quit(self):
+        self.plot_kill()
         self.wave_scope_kill()
         self.measure_kill()
         print ("Exiting.")
